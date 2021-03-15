d4dae9f3812e2543d06cb62521d8bcbc2522193e
Adam Dwi j <80248263+Chaplin213@users.noreply.github.com>
2021-03-15 20:05:14 +0700

    Add files via upload

---
+++ Minggu 2/Tugas/person.java
@@ -0,0 +1,20 @@
+
+package tugas;
+
+public class person {
+    public static void main(String[] args) {
+        
+        // initialize variable
+        String fName, lName, stuId, stuStatus;
+        fName = "Lisa";
+        lName = "Palombo";
+        stuId = "123456789";
+        stuStatus = "Active";
+        
+        // show
+        System.out.println("Student Name\t: " + fName + " " + lName);
+        System.out.println("Student ID\t: " + stuId);
+        System.out.println("Student Status\t: " + stuStatus);
+        
+    }
+}
