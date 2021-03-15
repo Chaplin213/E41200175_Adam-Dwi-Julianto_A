4ad1279f029ed16095a62d8f6e1ecb405b20386d
Adam Dwi j <80248263+Chaplin213@users.noreply.github.com>
2021-03-15 19:45:16 +0700

    Add files via upload

---
+++ Minggu 2/Latihan/Latihan_2.java
@@ -0,0 +1,17 @@
+package latihan;
+
+public class Latihan_2 {
+    public static void main(String[] args) {
+        
+        String nama = "Indonesia";
+        char abjad = 'a';
+        int a = 6;
+        double b = 6.9;
+        
+        System.out.println("Nama negara : " + nama);
+        
+        System.out.println(a);
+        
+        
+    }
+}
