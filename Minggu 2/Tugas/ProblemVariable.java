e19ad0cd31d1c03ec1d5bb2d09989a4725e141c2
Adam Dwi j <80248263+Chaplin213@users.noreply.github.com>
2021-03-15 19:46:57 +0700

    Add files via upload

---
+++ Minggu 2/Tugas/ProblemVariable.java
@@ -0,0 +1,22 @@
+
+package tugas;
+
+public class ProblemVariable {
+    public static void main(String[] args) {
+        
+        // variable yang benar (deklarasi)
+        boolean gameOver = false;
+        int students = 50, classes = 3;
+        double sales_tax;
+        short number1;
+        
+        // variable yang salah (deklarasi)
+        int 2beOrNot2be;
+        float price index;
+        double lastYear'sPrice;
+        long class;
+        
+        
+        
+    }
+}
