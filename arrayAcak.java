ba688e44a04318df2a2b6e9b6800ceea0069ae6c
Adam Dwi j <80248263+Chaplin213@users.noreply.github.com>
2021-03-23 08:41:53 +0700

    Add files via upload

---
+++ arrayAcak.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tugas;
+
+/**
+ *
+ * @author ADAM
+ */
+public class arrayAcak {
+    public static void main(String[] args){
+        int[] angka = {1,8,5,4,7,9,3,3,6};
+        
+        
+        System.out.println("Jemlah deret pada array = "+angka.length);
+    }
+    
+}
