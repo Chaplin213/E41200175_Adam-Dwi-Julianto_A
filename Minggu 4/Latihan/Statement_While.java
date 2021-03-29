6e9553ea3abfc12d2aa9658863dca771ad6939c4
Adam Dwi j <80248263+Chaplin213@users.noreply.github.com>
2021-03-29 17:42:57 +0700

    Add files via upload

---
+++ Minggu 4/Latihan/Statement_While.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan;
+
+/**
+ *
+ * @author ADAM
+ */
+public class Statement_While {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i = 0;
+        
+        while (i <= 10){
+            
+            System.out.println("Perulangan ke-" + i);
+            
+            i++;
+        }
+    }
+    
+}
