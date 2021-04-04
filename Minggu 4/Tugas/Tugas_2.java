608dfab7a894bada20298644b1494580fd36ae61
Adam Dwi j <80248263+Chaplin213@users.noreply.github.com>
2021-04-04 18:02:23 +0700

    Update Tugas_2.java

--- Minggu 4/Tugas/Tugas_2.java
+++ Minggu 4/Tugas/Tugas_2.java
@@ -13,7 +13,7 @@ public class Tugas_2 {
     public static void main(String[] args){
          int i = 1; 
          System.out.println("\t Do_While");
-         System.out.println("Bilangan kelipatan 2 (1-100)");
+         System.out.println("Bilangan kali 2 (1-100)");
          System.out.println("============================");
          do{ 
             System.out.print(" "+  i + " "); 
